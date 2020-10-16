48057db18dab41bbaebcfe76a13700209581394b
Brooke McBain <mcbainbrooke@gmail.com>
2020-10-15 21:13:29 -0500

    create, superhero, view, intitial classes created

---
+++ Controllers/SuperheroController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Superhero.Data;
+
+namespace Superhero.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        // GET: SuperheroController
+        private ApplicationDbContext _db;
+        public SuperheroController( ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                Superhero superhero1 = new Superhero();
+                _db.Superheroes.Add(superhero1);
+                _db.SaveChanges(); 
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
